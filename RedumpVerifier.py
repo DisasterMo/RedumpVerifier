3d4928c9c3fe8560765bbb380b4485318ab45d34
DisasterMo <michael.burgardt@gmail.com>
2023-07-11 23:23:33 +0200

    Split hash matching into its own function

--- RedumpVerifier.py
+++ RedumpVerifier.py
@@ -3,7 +3,6 @@ import hashlib
 import sys
 import datetime
 
-
 dats = os.listdir("./dat")
 read_size = 1024
 
@@ -27,9 +26,8 @@ def get_all_files(dir_name):
 
 
 def verify(files):
-    global romList, romListRedumpName, romListHash
+    global dats, romList, romListRedumpName, romListHash
     for file in files:
-        game_verified = False
         iso = file.replace("\"", "")
         print("\nISO: " + iso)
         hasher = hashlib.md5()
@@ -43,36 +41,11 @@ def verify(files):
 
         romList.append(file)
 
-        for dat in dats:
-            line_number = 0
-            data = ""
-            with open("dat/" + dat, "r") as f:
-                data = f.readlines()
-                for line in data:
-                    line_number += 1
-                    if md5hash in line:
-                        print("\n"
-                              + "ISO's MD5 hash: " + md5hash
-                              + "\n"
-                              + "Game Verified, ISO's MD5 matches Redump hash"
-                              )
-                        name_line = line_number
-                        while "<description>" not in data[name_line]:
-                            name_line -= 1
-                        game_name = data[name_line]\
-                            .replace("<description>", "")\
-                            .replace("</description>", "")\
-                            .replace("\t", "")
-                        print("Redump game name: " + game_name
-                              + "\n"
-                              + "----------------------------------------")
-                        game_verified = True
+        game_name = get_fast_match(dats, md5hash)
 
-            if game_verified:
-                romListRedumpName.append(game_name)
-                break
-
-        if not game_verified:
+        if game_name:
+            romListRedumpName.append(game_name)
+        else:
             print("ISO's MD5: " + md5hash
                   + "\n"
                   + "ISO's MD5 doesn't match any Redump hash"
@@ -85,16 +58,50 @@ def verify(files):
         romListHash.append(md5hash)
 
 
+def get_fast_match(dat_files, md5hash):
+    # Check dat files one by one.
+    for dat in dat_files:
+        line_number = 0
+        game_name = None
+        with open("dat/" + dat, "r") as f:
+            data = f.readlines()
+            for line in data:
+                line_number += 1
+                # If match found, print every match in this dat file,
+                # as sometimes multiple games share the same track.
+                # The hope is, that at least one of them will be the
+                # actual game in question.
+                if md5hash in line:
+                    name_line = line_number
+                    while "<description>" not in data[name_line]:
+                        name_line -= 1
+                    game_name = data[name_line] \
+                        .replace("<description>", "") \
+                        .replace("</description>", "") \
+                        .replace("\t", "")
+                    print("\n"
+                          + "ISO's MD5 hash: " + md5hash
+                          + "\n"
+                          + "Game Verified, ISO's MD5 matches Redump hash"
+                          + "\n"
+                          + f"Redump game name: {game_name} ({dat})"
+                          + "\n"
+                          + "----------------------------------------")
+        # Do not check any further dat files to reduce run time.
+        if game_name:
+            return game_name
+    return None
+
+
 # --------------------------------------------------------------------------- #
 # Check for updates #
 with open("dat/_last_update", "r") as f:
     last_update_time = f.read()
 
-
 if (int(str(datetime.date.today()).split("-")[1]) >
     int(last_update_time.split("-")[1])) \
-   | (int(str(datetime.date.today()).split("-")[0]) >
-      int(last_update_time.split("-")[0])):
+        | (int(str(datetime.date.today()).split("-")[0]) >
+           int(last_update_time.split("-")[0])):
     print("DATs haven't been updated in at least one month")
     i = input("Do you want to update them? (y/n) > ")
     if i == "y":
