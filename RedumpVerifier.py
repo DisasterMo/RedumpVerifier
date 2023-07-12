c0a9143bc4f74eddb87075cac8baf80ca5385fc5
DisasterMo <michael.burgardt@gmail.com>
2023-07-12 10:44:03 +0200

    Add 'best match' function for more accurate matching. Utilises the file name in addition to the hash.

--- RedumpVerifier.py
+++ RedumpVerifier.py
@@ -2,6 +2,7 @@ import os
 import hashlib
 import sys
 import datetime
+from difflib import SequenceMatcher
 
 dats = os.listdir("./dat")
 read_size = 1024
@@ -62,6 +63,28 @@ def verify(files):
         romListHash.append(md5hash)
 
 
+def print_match(md5hash, game_name, dat):
+    print("\n"
+          + "ISO's MD5 hash: " + md5hash
+          + "\n"
+          + "Game Verified, ISO's MD5 matches Redump hash"
+          + "\n"
+          + f"Redump game name: {game_name} ({dat})"
+          + "\n"
+          + "----------------------------------------")
+
+
+def get_game_name(data, name_line):
+    while "<description>" not in data[name_line]:
+        name_line -= 1
+    game_name = data[name_line] \
+        .replace("<description>", "") \
+        .replace("</description>", "") \
+        .replace("\t", "")
+
+    return game_name
+
+
 def get_fast_match(dat_files, md5hash):
     # Check dat files one by one.
     for dat in dat_files:
@@ -76,25 +99,38 @@ def get_fast_match(dat_files, md5hash):
                 # The hope is, that at least one of them will be the
                 # actual game in question.
                 if md5hash in line:
-                    name_line = line_number
-                    while "<description>" not in data[name_line]:
-                        name_line -= 1
-                    game_name = data[name_line] \
-                        .replace("<description>", "") \
-                        .replace("</description>", "") \
-                        .replace("\t", "")
-                    print("\n"
-                          + "ISO's MD5 hash: " + md5hash
-                          + "\n"
-                          + "Game Verified, ISO's MD5 matches Redump hash"
-                          + "\n"
-                          + f"Redump game name: {game_name} ({dat})"
-                          + "\n"
-                          + "----------------------------------------")
+                    game_name = get_game_name(data, line_number)
+                    print_match(md5hash, game_name, dat)
         # Do not check any further dat files to reduce run time.
         if game_name:
             return game_name
     return None
+
+
+def get_best_match(dat_files, md5hash, file_name):
+    best_match_score = 0
+    best_name = None
+
+    # Check all dat files
+    # TODO: If this turns out to be too slow, it can be parallelised. Alternative similarity algorithms (Levenshtein) are also an option.
+    for dat in dat_files:
+        line_number = 0
+        with open("dat/" + dat, "r") as f:
+            data = f.readlines()
+            for line in data:
+                line_number += 1
+                if md5hash in line:
+                    game_name = get_game_name(data, line_number)
+                    # Get similarity score for the match
+                    local_score = SequenceMatcher(None, file_name, game_name).ratio()
+                    # The higher the score, the more similar the names
+                    if local_score > best_match_score:
+                        best_match_score = local_score
+                        best_name = game_name
+                        # Only print better names, to reduce spam
+                        print_match(md5hash, game_name, dat)
+    # Return either the best name match, or None
+    return best_name
 
 
 # --------------------------------------------------------------------------- #
@@ -131,6 +167,7 @@ else:
     userInput = input("> ")
     input_list = userInput.split("*")
 
+# Verify #
 for i in input_list:
     path = prepare_path(i)
     if os.path.isfile(path):  # if input is a file
